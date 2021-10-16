d9058d5eecaca952bc454d12bfb15c783de7d520
Niccari <latieos93@gmail.com>
2021-10-16 13:21:28 +0900

    Fix: webpack.web.js og:image extension

--- webpack.web.js
+++ webpack.web.js
@@ -15,7 +15,7 @@ module.exports = {
         'og:description': { property: 'og:description', content: 'WWAゲームについて、マップ画像を出力するツール' },
         'og:type': { property: 'og:type', content: 'website' },
         'og:url': { property: 'og:url', content: 'https://niccari.net/wwamap/' },
-        'og:image': { property: 'og:image', content: 'https://niccari.net/wwamap/ogp.png' },
+        'og:image': { property: 'og:image', content: 'https://niccari.net/wwamap/ogp.jpg' },
         'twitter:card': { name: 'twitter:card', content: 'summary_large_image' },
         'twitter:title': { name: 'twitter:title', content: 'WWAゲーム マップ画像出力ツール' },
         'twitter:description': { name: 'twitter:description', content: 'WWAゲームについて、マップ画像を出力するツール' },
